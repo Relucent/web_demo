4ac32c4e2fd5c2d52a0077f2034dad1e393a0013
relucent <relucent@163.com>
2017-05-24 15:44:07 +0800

    @

---
+++ demo/node/server1.js
@@ -0,0 +1,11 @@
+var PORT = 2000;
+var http = require('http');
+var server = http.createServer(function (request, response) {
+	response.writeHead(200, {
+		'Content-Type': 'text/plain'
+	});
+	response.write('hello world');
+	response.end();
+});
+server.listen(PORT);
+console.log('Server runing at port: ' + PORT + '.');
